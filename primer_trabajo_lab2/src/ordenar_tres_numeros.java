1cefe38ad50c54a817ca10e1a77ddfac65f59263
GabrielBarrios17 <matiaspuntocom@outlook.com>
2023-08-11 12:06:06 -0300

    primer trabajo

---
+++ primer_trabajo_lab2/src/ordenar_tres_numeros.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ordenar_tres_numeros {
+    public static void main(String[] arg){
+        Scanner s = new Scanner(System.in);
+        int[] numeros = new int[4];
+
+        System.out.println("ingrese el primer numero");
+        numeros[0] = s.nextInt();
+        System.out.println("ingrese el segundo numero");
+        numeros[1] = s.nextInt();
+        System.out.println("ingrese el tercer numero");
+        numeros[2] = s.nextInt();
+        for (int i = 0; i < 4 ; i++) {
+            for (int j = 1; j < 4; j++) {
+                if (numeros[i] < numeros[j]) {
+                    numeros[3] = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = numeros[3];
+                }
+            }
+        }
+        System.out.println(numeros[0]+" > "+numeros[1]+" > "+numeros[2]);
+    }
+}
